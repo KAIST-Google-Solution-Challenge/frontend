e853bda11ebf8d202aeec0d311731076407a5b4a
Byunk <clearman001@kaist.ac.kr>
2023-03-23 23:57:29 +0900

    Fix: old android no file found error

--- lib/util/constant.dart
+++ lib/util/constant.dart
@@ -1,6 +1,7 @@
 const String BASEURL = 'http://35.216.72.5:3000/';
 // const String BASEURL = 'https://eac9-110-76-108-201.jp.ngrok.io';
 // const String BASEURL = 'http://10.0.2.2:3000'; // For Android emulator
-const double THREASHOLD = 50;
+const double THREASHOLD = 80;
 
 const String EngDir = '/storage/emulated/0/Recordings/Call';
+const String EngDirOld = '/storage/emulated/0/Call';
